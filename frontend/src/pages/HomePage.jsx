d3f89cf3446d8baf46836ae268835ac60aa98676
vasudhiman12 <vasudhiman12@gmail.com>
2025-06-14 15:53:56 +0530

    prepared for deployment

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,7 +4,7 @@ import RateLimitedUI from "../components/RateLimitedUI";
 import { useEffect } from "react";
 import api from "../lib/axios";
 import toast from "react-hot-toast";
-import NoteCard from "../components/Notecard";
+import NoteCard from "../components/NoteCard";
 import NotesNotFound from "../components/NotesNotFound";
 
 const HomePage = () => {
